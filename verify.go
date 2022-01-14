16a58a6388d5f15f4a76d56823621c0335789096
Connor Finnell <connor.finnell@udacity.com>
2022-01-14 14:28:41 -0700

    feat: Parallel exec framework for generating hashes

--- verify.go
+++ verify.go
@@ -7,8 +7,33 @@ import (
 
 func Verify(targets []pgx.ConnConfig, includeTables, excludeTables []string) error {
 	log.Infof("Verifying %d targets", len(targets))
+
+	// First check that we can connect to every specified target database.
+	conns := make(map[int]*pgx.Conn)
 	for i, target := range targets {
-		log.Infof("[%d] %s", i, target.Host)
+		conn, err := pgx.Connect(target)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+		conns[i] = conn
 	}
+
+	// Then query each target database in parallel to generate table hashes.
+	var doneChannels []chan struct{}
+	for i, conn := range conns {
+		done := make(chan struct{})
+		go generateTableHashes(i, conn, includeTables, excludeTables, done)
+		doneChannels = append(doneChannels, done)
+	}
+	for _, done := range doneChannels {
+		<-done
+	}
+
 	return nil
+}
+
+func generateTableHashes(targetIndex int, conn *pgx.Conn, explicitTables, excludeTables []string, done chan struct{}) {
+	// TODO
+	close(done)
 }
