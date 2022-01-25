59bf5c9132e8a1e42a69ad3aec58e131048bbaf8
Connor Finnell <connor.finnell@udacity.com>
2022-01-25 08:42:33 -0700

    nit: Adjust CLI usage messages

--- cmd/pgverify/cmd.go
+++ cmd/pgverify/cmd.go
@@ -21,14 +21,14 @@ var (
 
 func init() {
 	aliasesFlag = rootCmd.Flags().StringSlice("aliases", []string{}, "alias names for the supplied targets (comma separated)")
-	targetsFlag = rootCmd.Flags().StringSliceP("targets", "t", []string{}, "target postgresql-format database URIs (comma separated)")
+	targetsFlag = rootCmd.Flags().StringSliceP("targets", "t", []string{}, "REQUIRED: target postgresql-format database URIs (comma separated)")
 	rootCmd.MarkFlagRequired("targets") //nolint:errcheck
 
 	includeTablesFlag = rootCmd.Flags().StringSlice("include-tables", []string{}, "tables to verify (comma separated)")
 	excludeTablesFlag = rootCmd.Flags().StringSlice("exclude-tables", []string{}, "tables to skip verification, ignored if '--include-tables' used (comma separated)")
 	includeSchemasFlag = rootCmd.Flags().StringSlice("include-schemas", []string{}, "schemas to verify (comma separated)")
 	excludeSchemasFlag = rootCmd.Flags().StringSlice("exclude-schemas", []string{}, "schemas to skip verification, ignored if '--include-schemas' used (comma separated)")
-	strategyFlag = rootCmd.Flags().StringP("strategy", "s", pgverify.StrategyFull, "strategy to use for verification (default: 'full')")
+	strategyFlag = rootCmd.Flags().StringP("strategy", "s", pgverify.StrategyFull, "strategy to use for verification")
 }
 
 var rootCmd = &cobra.Command{
