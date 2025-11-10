940d2350c779948d6ed5469bba35c5e945d1f0e0
Connor Finnell <contact@connorfinnell.com>
2025-11-10 09:20:32 -0700

    test: Cleaner crdb/psql version specification

--- integration_test.go
+++ integration_test.go
@@ -89,179 +89,206 @@ func TestVerifyData(t *testing.T) {
 		t.Skip("Skipping integration test")
 	}
 
-	// Arrange
-	ctx := t.Context()
+	for _, tc := range []struct {
+		name         string
+		psqlVersions []string
+		crdbVersions []string
+	}{
+		{
+			name:         "Latest",
+			psqlVersions: []string{"latest"},
+			crdbVersions: []string{"latest"}, // cockroach cloud
+		},
+		{
+			name: "Full",
+			psqlVersions: []string{
+				"10",
+				"11",
+				"12",
+				"13",
+				"14",
+				"15",
+				"16",
+				"17",
+				"18",
+			},
+			crdbVersions: []string{
+				"latest-v22.2",
+				"latest-v23.2",
+				"latest-v24.3",
+				"latest-v25.3",
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+			dbs := make([]string, 0, len(tc.psqlVersions)+len(tc.crdbVersions))
 
-	dbs := []string{
-		"postgres:10",
-		"postgres:11",
-		"postgres:12",
-		"postgres:13",
-		"postgres:14",
-		"postgres:15",
-		"postgres:16",
-		"postgres:17",
-		"postgres:latest",
-		"cockroachdb/cockroach:latest-v22.2",
-		"cockroachdb/cockroach:latest-v23.2",
-		"cockroachdb/cockroach:latest-v24.3",
-		"cockroachdb/cockroach:latest-v25.2",
-		"cockroachdb/cockroach:latest", // cockroach cloud
-	}
+			for _, tag := range tc.psqlVersions {
+				dbs = append(dbs, "postgres:"+tag)
+			}
 
-	columnTypes := map[string][]string{
-		"boolean":   {"true", "false"},
-		"bytea":     {fmt.Sprintf("'%s'", hex.EncodeToString([]byte("convert this content to bytes")))},
-		"bit(1)":    {"'1'", "'0'"},
-		"varbit(3)": {"'0'", "'1'", "'101'", "'010'"},
+			for _, tag := range tc.crdbVersions {
+				dbs = append(dbs, "cockroachdb/cockroach:"+tag)
+			}
 
-		"bigint[]":         {"'{602213950000000000, -1}'", "'{}'", "ARRAY[]::bigint[]"},
-		"integer":          {"0", "123979", "-23974"},
-		"double precision": {"69.123987", "-69.123987"},
-		"numeric":          {"0", "123.456", "-123.456"},
-		"decimal":          {"0", "123.456", "-123.456"},
+			columnTypes := map[string][]string{
+				"boolean":   {"true", "false"},
+				"bytea":     {fmt.Sprintf("'%s'", hex.EncodeToString([]byte("convert this content to bytes")))},
+				"bit(1)":    {"'1'", "'0'"},
+				"varbit(3)": {"'0'", "'1'", "'101'", "'010'"},
 
-		"text":                  {`'foo'`, `'bar'`, `''`, `'something that is much longer but without much other complication'`},
-		"uuid":                  {fmt.Sprintf("'%s'", uuid.New().String())},
-		`character varying(64)`: {`'more string stuff'`},
-		"text[]":                {`'{"foo", "bar"}'`, `'{"baz", "qux"}'`, `'{"foo", "bar", "baz", "qux"}'`, `ARRAY[]::text[]`, `'{}'`},
+				"bigint[]":         {"'{602213950000000000, -1}'", "'{}'", "ARRAY[]::bigint[]"},
+				"integer":          {"0", "123979", "-23974"},
+				"double precision": {"69.123987", "-69.123987"},
+				"numeric":          {"0", "123.456", "-123.456"},
+				"decimal":          {"0", "123.456", "-123.456"},
 
-		"jsonb": {`'{}'`, `'{"foo":["bar","baz"]}'`, `'{"foo": "bar"}'`, `'{"foo": "bar", "baz": "qux"}'`, `'{"for sure?": true, "has numbers": 123.456, "this is": ["some", "json", "blob"]}'`},
-		"json":  {`'{}'`, `'{"foo":["bar","baz"]}'`, `'{"foo": "bar"}'`, `'{"foo": "bar", "baz": "qux"}'`, `'{"for sure?": true, "has numbers": 123.456, "this is": ["some", "json", "blob"]}'`},
+				"text":                  {`'foo'`, `'bar'`, `''`, `'something that is much longer but without much other complication'`},
+				"uuid":                  {fmt.Sprintf("'%s'", uuid.New().String())},
+				`character varying(64)`: {`'more string stuff'`},
+				"text[]":                {`'{"foo", "bar"}'`, `'{"baz", "qux"}'`, `'{"foo", "bar", "baz", "qux"}'`, `ARRAY[]::text[]`, `'{}'`},
 
-		"date":                        {`'2020-12-31'`},
-		"timestamp with time zone":    {`'2020-12-31 23:59:59 -8:00'`, `'2022-06-08 20:03:06.957223+00'`}, // hashes differently for psql/crdb, convert to epoch when hashing
-		"timestamp without time zone": {`'2020-12-31 23:59:59'`},
-	}
-	keys := make([]string, len(columnTypes))
-	keysWithTypes := make([]string, len(columnTypes))
-	sortedTypes := make([]string, len(columnTypes))
-	i := 0
+				"jsonb": {`'{}'`, `'{"foo":["bar","baz"]}'`, `'{"foo": "bar"}'`, `'{"foo": "bar", "baz": "qux"}'`, `'{"for sure?": true, "has numbers": 123.456, "this is": ["some", "json", "blob"]}'`},
+				"json":  {`'{}'`, `'{"foo":["bar","baz"]}'`, `'{"foo": "bar"}'`, `'{"foo": "bar", "baz": "qux"}'`, `'{"for sure?": true, "has numbers": 123.456, "this is": ["some", "json", "blob"]}'`},
 
-	for k := range columnTypes {
-		// Create sanitized column name from type
-		cleanName := strings.ReplaceAll("col_"+k, " ", "_")
-		for _, char := range "()[]" {
-			cleanName = strings.ReplaceAll(cleanName, string(char), "X")
-		}
+				"date":                        {`'2020-12-31'`},
+				"timestamp with time zone":    {`'2020-12-31 23:59:59 -8:00'`, `'2022-06-08 20:03:06.957223+00'`}, // hashes differently for psql/crdb, convert to epoch when hashing
+				"timestamp without time zone": {`'2020-12-31 23:59:59'`},
+			}
+			keys := make([]string, len(columnTypes))
+			keysWithTypes := make([]string, len(columnTypes))
+			sortedTypes := make([]string, len(columnTypes))
+			i := 0
 
-		sortedTypes[i] = k
-		keys[i] = cleanName
-		keysWithTypes[i] = strings.Join([]string{keys[i], k}, " ")
-		i++
-	}
+			for k := range columnTypes {
+				// Create sanitized column name from type
+				cleanName := strings.ReplaceAll("col_"+k, " ", "_")
+				for _, char := range "()[]" {
+					cleanName = strings.ReplaceAll(cleanName, string(char), "X")
+				}
 
-	sort.Strings(keys)
-	sort.Strings(keysWithTypes)
-	sort.Strings(sortedTypes)
+				sortedTypes[i] = k
+				keys[i] = cleanName
+				keysWithTypes[i] = strings.Join([]string{keys[i], k}, " ")
+				i++
+			}
 
-	tableNames := []string{"testtable1", "testTABLE_multi_col_2", "testtable3", "test_stringkey_table4", "test_column_names"}
-	createTableQueryBase := fmt.Sprintf("( id INT DEFAULT 0 NOT NULL, zid INT DEFAULT 0 NOT NULL, sid TEXT NOT NULL, ignored TIMESTAMP WITH TIME ZONE DEFAULT NOW(), %s);", strings.Join(keysWithTypes, ", "))
+			sort.Strings(keys)
+			sort.Strings(keysWithTypes)
+			sort.Strings(sortedTypes)
 
-	rowCount := calculateRowCount(columnTypes)
-	insertDataQueryBase := `(id, zid, sid,` + strings.Join(keys, ", ") + `) VALUES `
-	valueClauses := make([]string, 0, rowCount)
+			tableNames := []string{"testtable1", "testTABLE_multi_col_2", "testtable3", "test_stringkey_table4", "test_column_names"}
+			createTableQueryBase := fmt.Sprintf("( id INT DEFAULT 0 NOT NULL, zid INT DEFAULT 0 NOT NULL, sid TEXT NOT NULL, ignored TIMESTAMP WITH TIME ZONE DEFAULT NOW(), %s);", strings.Join(keysWithTypes, ", "))
 
-	// Modulo-cycle through prefixes to re-create ORDER BY issue
-	textPKeyPrefixes := []string{"A", "AA", "a", "aa", "A-A", "a-a"}
+			rowCount := calculateRowCount(columnTypes)
+			insertDataQueryBase := `(id, zid, sid,` + strings.Join(keys, ", ") + `) VALUES `
+			valueClauses := make([]string, 0, rowCount)
 
-	for rowID := range rowCount {
-		textPKeyPrefix := textPKeyPrefixes[rowID%len(textPKeyPrefixes)]
-		valueClause := fmt.Sprintf("( %d, 0, '%s-%d'", rowID, textPKeyPrefix, rowID)
+			// Modulo-cycle through prefixes to re-create ORDER BY issue
+			textPKeyPrefixes := []string{"A", "AA", "a", "aa", "A-A", "a-a"}
 
-		var valueClauseSb167 strings.Builder
-		for _, columnType := range sortedTypes {
-			valueClauseSb167.WriteString(`, ` + columnTypes[columnType][rowID%len(columnTypes[columnType])])
-		}
+			for rowID := range rowCount {
+				textPKeyPrefix := textPKeyPrefixes[rowID%len(textPKeyPrefixes)]
+				valueClause := fmt.Sprintf("( %d, 0, '%s-%d'", rowID, textPKeyPrefix, rowID)
 
-		valueClause += valueClauseSb167.String()
+				var valueClauseSb167 strings.Builder
+				for _, columnType := range sortedTypes {
+					valueClauseSb167.WriteString(`, ` + columnTypes[columnType][rowID%len(columnTypes[columnType])])
+				}
 
-		valueClause += `)`
+				valueClause += valueClauseSb167.String()
 
-		valueClauses = append(valueClauses, valueClause)
-	}
+				valueClause += `)`
 
-	// Act
-	var targets []*pgx.ConnConfig
+				valueClauses = append(valueClauses, valueClause)
+			}
 
-	var aliases []string
+			// Act
+			ctx := t.Context()
 
-	for _, dbImage := range dbs {
-		aliases = append(aliases, dbImage)
-		// Create db and connect
-		config, err := createContainer(t, ctx, dbImage)
-		require.NoError(t, err)
-		assert.True(t, waitForDBReady(t, ctx, config))
-		conn, err := pgx.ConnectConfig(ctx, config)
-		require.NoError(t, err)
+			var targets []*pgx.ConnConfig
 
-		defer conn.Close(ctx)
+			var aliases []string
 
-		// Create and populate tables
-		for _, tableName := range tableNames {
-			if tableName == "test_column_names" {
-				_, err = conn.Exec(ctx, `
+			for _, dbImage := range dbs {
+				aliases = append(aliases, dbImage)
+				// Create db and connect
+				config, err := createContainer(t, ctx, dbImage)
+				require.NoError(t, err)
+				assert.True(t, waitForDBReady(t, ctx, config))
+				conn, err := pgx.ConnectConfig(ctx, config)
+				require.NoError(t, err)
+
+				defer conn.Close(ctx)
+
+				// Create and populate tables
+				for _, tableName := range tableNames {
+					if tableName == "test_column_names" {
+						_, err = conn.Exec(ctx, `
 				CREATE TABLE test_column_names (
 					id INT PRIMARY KEY,
 					"default" INT,
 					"order" INT
 				);`)
-				require.NoError(t, err, "Failed to initialize 'test_column_names' table")
+						require.NoError(t, err, "Failed to initialize 'test_column_names' table")
 
-				continue // skip populating this table
+						continue // skip populating this table
+					}
+
+					createTableQuery := fmt.Sprintf(`CREATE TABLE "%s" %s`, tableName, createTableQueryBase)
+					_, err = conn.Exec(ctx, createTableQuery)
+					require.NoError(t, err, "Failed to create table %s on %v with query: %s", tableName, dbImage, createTableQuery)
+
+					var pkeyString string
+
+					switch {
+					case strings.Contains(tableName, "multi_col"):
+						pkeyString = fmt.Sprintf("multi_col_pkey_%s PRIMARY KEY (id, zid)", tableName)
+					case strings.Contains(tableName, "stringkey"):
+						pkeyString = fmt.Sprintf("text_col_pkey_%s PRIMARY KEY (sid)", tableName)
+					default:
+						pkeyString = fmt.Sprintf("single_col_pkey_%s PRIMARY KEY (id)", tableName)
+					}
+
+					require.NotEmpty(t, pkeyString)
+
+					alterTableQuery := fmt.Sprintf(`ALTER TABLE ONLY "%s" ADD CONSTRAINT %s;`, tableName, pkeyString)
+					_, err = conn.Exec(ctx, alterTableQuery)
+					require.NoError(t, err, "Failed to add primary key to table %s on %v with query %s", tableName, dbImage, alterTableQuery)
+
+					rand.Shuffle(len(valueClauses), func(i, j int) { valueClauses[i], valueClauses[j] = valueClauses[j], valueClauses[i] })
+					insertDataQuery := fmt.Sprintf(`INSERT INTO "%s" %s %s`, tableName, insertDataQueryBase, strings.Join(valueClauses, ", "))
+					_, err = conn.Exec(ctx, insertDataQuery)
+					require.NoError(t, err, "Failed to insert data to table on %v with query %s", tableName, dbImage, insertDataQuery)
+				}
+
+				targets = append(targets, config)
 			}
 
-			createTableQuery := fmt.Sprintf(`CREATE TABLE "%s" %s`, tableName, createTableQueryBase)
-			_, err = conn.Exec(ctx, createTableQuery)
-			require.NoError(t, err, "Failed to create table %s on %v with query: %s", tableName, dbImage, createTableQuery)
+			logger := logrus.New()
+			logger.Level = logrus.ErrorLevel
 
-			var pkeyString string
-
-			switch {
-			case strings.Contains(tableName, "multi_col"):
-				pkeyString = fmt.Sprintf("multi_col_pkey_%s PRIMARY KEY (id, zid)", tableName)
-			case strings.Contains(tableName, "stringkey"):
-				pkeyString = fmt.Sprintf("text_col_pkey_%s PRIMARY KEY (sid)", tableName)
-			default:
-				pkeyString = fmt.Sprintf("single_col_pkey_%s PRIMARY KEY (id)", tableName)
-			}
-
-			require.NotEmpty(t, pkeyString)
-
-			alterTableQuery := fmt.Sprintf(`ALTER TABLE ONLY "%s" ADD CONSTRAINT %s;`, tableName, pkeyString)
-			_, err = conn.Exec(ctx, alterTableQuery)
-			require.NoError(t, err, "Failed to add primary key to table %s on %v with query %s", tableName, dbImage, alterTableQuery)
-
-			rand.Shuffle(len(valueClauses), func(i, j int) { valueClauses[i], valueClauses[j] = valueClauses[j], valueClauses[i] })
-			insertDataQuery := fmt.Sprintf(`INSERT INTO "%s" %s %s`, tableName, insertDataQueryBase, strings.Join(valueClauses, ", "))
-			_, err = conn.Exec(ctx, insertDataQuery)
-			require.NoError(t, err, "Failed to insert data to table on %v with query %s", tableName, dbImage, insertDataQuery)
-		}
-
-		targets = append(targets, config)
+			results, err := pgverify.Verify(
+				ctx,
+				targets,
+				pgverify.WithTests(
+					pgverify.TestModeBookend,
+					pgverify.TestModeSparse,
+					pgverify.TestModeFull,
+					pgverify.TestModeRowCount,
+				),
+				pgverify.WithLogger(logger),
+				pgverify.ExcludeSchemas("pg_catalog", "pg_extension", "information_schema", "crdb_internal"),
+				pgverify.ExcludeColumns("ignored", "rowid"),
+				pgverify.WithAliases(aliases),
+				pgverify.WithBookendLimit(5),
+				pgverify.WithHashPrimaryKeys(),
+			)
+			require.NoError(t, err)
+			require.NoError(t, results.WriteAsTable(os.Stdout))
+		})
 	}
-
-	logger := logrus.New()
-	logger.Level = logrus.ErrorLevel
-
-	results, err := pgverify.Verify(
-		ctx,
-		targets,
-		pgverify.WithTests(
-			pgverify.TestModeBookend,
-			pgverify.TestModeSparse,
-			pgverify.TestModeFull,
-			pgverify.TestModeRowCount,
-		),
-		pgverify.WithLogger(logger),
-		pgverify.ExcludeSchemas("pg_catalog", "pg_extension", "information_schema", "crdb_internal"),
-		pgverify.ExcludeColumns("ignored", "rowid"),
-		pgverify.WithAliases(aliases),
-		pgverify.WithBookendLimit(5),
-		pgverify.WithHashPrimaryKeys(),
-	)
-	require.NoError(t, err)
-	require.NoError(t, results.WriteAsTable(os.Stdout))
 }
 
 func TestVerifyDataFail(t *testing.T) {
